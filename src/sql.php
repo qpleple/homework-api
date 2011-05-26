f89164f78c4527208807f0eee7e6fda8816acfec
Quentin Pleplé <quentin.pleple@gmail.com>
2011-05-25 19:31:11 -0700

    sql debug

--- src/sql.php
+++ src/sql.php
@@ -6,12 +6,12 @@ class SQL {
     static $bdd;
 
     static public function open($db_server, $db_port, $db_user, $db_passwd, $db_name) {
+        $db_connection = "mysql:host=$db_server;port=$db_port;dbname=$db_name";
         try {
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            
-            self::$bdd = new PDO("mysql:host=$db_server;port=$db_port;dbname=$db_name", $db_user, $db_passwd, $pdo_options);
+            self::$bdd = new PDO($db_connection, $db_user, $db_passwd, $pdo_options);
         } catch (Exception $e){
-            die($e->getMessage());
+            die($e->getMessage() . "<br> $db_connection");
             //die("Database configuration problem.");
         }
     }
