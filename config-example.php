b88e8174f851c08d980325acb4911e6af9bbad52
Quentin Pleplé <quentin.pleple@gmail.com>
2011-05-26 07:59:24 -0700

    Set dumb values to example config.php

---
+++ config-example.php
@@ -0,0 +1,17 @@
+<?php
+// Server Key (here : http://www.mashape.com/account/apis/created )
+$server_key = "your-server-key";
+
+// Database settings
+$db_server = "localhost";
+$db_port = "3306";
+$db_user = "root";
+$db_passwd = "root";
+$db_name = "homeworkapi";
+
+
+// Gmail settings for new homework notification
+$gmail_username = 'username@gmail.com'; 
+$gmail_password = 'password';
+$gmail_from = 'from@gmail.com'; 
+$gmail_to = 'to@gmail.com';
