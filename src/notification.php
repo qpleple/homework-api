15cd9932158bbacac446c293fcae977ba041c58f
Quentin Pleplé <quentin.pleple@gmail.com>
2011-05-26 05:14:21 -0700

    Removed gmail credentials (and having my gmail passwd changed ;p)

--- src/notification.php
+++ src/notification.php
@@ -1,13 +1,13 @@
 <?php
 function homeworkSubmittedNotification($id) {
     $mail = new PHPGMailer();
-    $mail->Username = 'qpleple@gmail.com'; 
-    $mail->Password = 'Aij!y48u59*3|SA(Cz~39{p[~9iU';
-    $mail->From = 'qpleple@gmail.com'; 
+    $mail->Username = 'yourgmailaccount@gmail.com'; 
+    $mail->Password = 'yourgmailpassword';
+    $mail->From = 'from-address@gmail.com'; 
     $mail->FromName = 'Homework API';
     $mail->Subject = '[Homework] New one !';
-    $mail->AddAddress('quentin.pleple@gmail.com');
-    $mail->Body = "You have work to do !\n  * Status : http://homeworkapi/status/$id\n  * Solution : http://homeworkapi/status/$id";
+    $mail->AddAddress('to-address@gmail.com');
+    $mail->Body = "You have work to do !\nid = $id";
     $mail->Send();
 }
 
