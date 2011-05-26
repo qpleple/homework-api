c94e90f7472f2688adcd2b8721c0a86728408cae
Quentin Pleplé <quentin.pleple@gmail.com>
2011-05-25 19:54:12 -0700

    sql debug

--- src/config.php
+++ src/config.php
@@ -5,12 +5,11 @@ $db_port = "3306";
 $db_user = "root";
 $db_passwd = "root";
 $db_name = "homeworkapi";
-
-if ($_SERVER['SERVER_NAME'] =! "homeworkapi") {
+die ($_SERVER['SERVER_NAME']);
+if ($_SERVER['SERVER_NAME'] == "homeworkapi") {
     $db_server = "db.homeworkapi.dotcloud.com";
     $db_port = "5581";
     $db_user = "root";
     $db_passwd = '+qwE-CD8nt>!N(%^C6D4';
     $db_name = "homeworkapi";
 }
-
